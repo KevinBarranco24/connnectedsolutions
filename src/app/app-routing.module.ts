260252f9a347bbdbec3989cf5097103e1fda234b
kevin <kevin.morales@lumno.udg.mx>
2022-03-07 00:09:46 -0600

    Primera version del front CS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ContactComponent } from './contact/contact.component';
+import { CreateComponent } from './create/create.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'Nosotros', component: AboutComponent
+  },
+  {
+    path: 'Contacto', component: ContactComponent
+  },
+  {
+    path: 'Categorias', component: CategoriesComponent
+  },
+  {
+    path: 'Tickets', component: TicketListComponent
+  },
+  {
+    path: 'Crear-TIcket', component: CreateComponent
+  },
+  {
+    path: '**', component: PagenotfoundComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ContactComponent } from './contact/contact.component';
+import { CreateComponent } from './create/create.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PagenotfoundComponent,
+    ContactComponent,
+    CreateComponent,
+    TicketListComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
